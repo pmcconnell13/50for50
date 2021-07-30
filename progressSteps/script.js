b474d10a54a610a8411035182b4aed2bc5f74a5a
Patrick McConnell <pmcconnell13@gmail.com>
2021-07-30 13:10:32 -0700

    project complete

--- progressSteps/script.js
+++ progressSteps/script.js
@@ -28,8 +28,24 @@ prev.addEventListener('click', () => {
 
 
 
-// function update() {
-//   circles.forEach((circle, id) => {
-//     if (id < currentActive)
-//   })
-// }
+function update() {
+  circles.forEach((circle, id) => {
+    if (id < currentActive) {
+      circle.classList.add('active');
+    } else {
+      circle.classList.remove('active')
+    }
+  })
+
+  const actives = document.querySelectorAll('.active')
+  progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+
+  if (currentActive === 1) {
+    prev.disabled = true;
+  } else if (currentActive === circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+}

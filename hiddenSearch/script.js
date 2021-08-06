d1afae07fb4e9d5eaa3c18fd0fea6276d5604ab9
Patrick McConnell <pmcconnell13@gmail.com>
2021-08-06 13:05:41 -0700

    faq

--- hiddenSearch/script.js
+++ hiddenSearch/script.js
@@ -1,7 +1,6 @@
 let search = document.querySelector('.search');
 let btn = document.querySelector('.btn');
 let input = document.querySelector('.input');
-let active = false;
 
 btn.addEventListener('click', () => {
   // if (active) {

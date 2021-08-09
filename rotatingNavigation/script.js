2cec11e47f3cf04517dc674c89e29e3e99d58dfd
Patrick McConnell <pmcconnell13@gmail.com>
2021-08-09 15:08:51 -0700

    script

--- rotatingNavigation/script.js
+++ rotatingNavigation/script.js
@@ -1,7 +1,7 @@
 let close = document.getElementById('close');
 let open = document.getElementById('open');
 let container = document.querySelector('.container');
-console.log(container)
+
 
 open.addEventListener('click', () => {
   container.classList.add('show-nav');

22069b94e97976288a8d5ff1a735fa64480f3de9
Patrick McConnell <pmcconnell13@gmail.com>
2021-08-10 11:17:22 -0700

    scroll html CSS

---
+++ scrollAnimation/script.js
@@ -0,0 +1,11 @@
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkboxes);
+
+function checkBoxes() {
+   const triggerBottom = window.innerHeight / 5 * 4;
+
+   boxes.forEach(box => {
+     
+   })
+}
